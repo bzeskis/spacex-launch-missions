c97c154076f9908eb1b4835ab9527913156a7472
issaknife <53504667+issaknife@users.noreply.github.com>
2020-09-23 17:59:23 +0300

    continuous build

--- server.js
+++ server.js
@@ -17,11 +17,13 @@ app.use(
     }),
 );
 
-app.use(express.static('public'))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-})
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 
